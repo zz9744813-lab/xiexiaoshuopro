5b83066a76f3a6ed30440bfbac5fa25572ca1351
zz9744813-lab <zz9744813@gmail.com>
2026-05-12 21:45:35 +0800

    TASK-10: create index.ts

---
+++ src/db/queries/index.ts
@@ -0,0 +1,7 @@
+// db/queries/index.ts — 查询层导出
+export * from "./chapter";
+export * from "./character";
+export * from "./world";
+export * from "./simulation";
+export * from "./issue";
+export * from "./job";
