6557472745156f8ee14a95ff3df8cf1bb84fb937
zz9744813-lab <zz9744813@gmail.com>
2026-05-13 03:52:21 +0800

    TASK-G: create IssueSidebar.tsx

---
+++ src/components/editor/IssueSidebar.tsx
@@ -0,0 +1,6 @@
+'use client'
+import React from 'react'
+
+export default function IssueSidebar({ chapterId, projectId }: { chapterId: string; projectId: string }) {
+  return <aside className="issue-sidebar">Issue Sidebar</aside>
+}
