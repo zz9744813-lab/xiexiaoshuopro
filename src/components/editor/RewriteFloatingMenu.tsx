7e21995e0a9b2938b9858aeb591568e127eab281
zz9744813-lab <zz9744813@gmail.com>
2026-05-13 03:52:25 +0800

    TASK-G: create RewriteFloatingMenu.tsx

---
+++ src/components/editor/RewriteFloatingMenu.tsx
@@ -0,0 +1,6 @@
+'use client'
+import React from 'react'
+
+export default function RewriteFloatingMenu({ selectedText, onRewrite }: { selectedText: string; onRewrite: (text: string) => void }) {
+  return <div className="rewrite-menu">Rewrite Menu</div>
+}
