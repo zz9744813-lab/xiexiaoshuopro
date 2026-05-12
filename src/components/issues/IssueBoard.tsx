df4b6cee9f5863cde25b6f93d51a8dcc3749bec9
zz9744813-lab <zz9744813@gmail.com>
2026-05-13 03:52:28 +0800

    TASK-G: create IssueBoard.tsx

---
+++ src/components/issues/IssueBoard.tsx
@@ -0,0 +1,6 @@
+'use client'
+import React from 'react'
+
+export default function IssueBoard({ projectId }: { projectId: string }) {
+  return <div className="issue-board">Issue Board - Kanban</div>
+}
