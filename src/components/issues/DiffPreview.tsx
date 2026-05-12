882a260707c111dbbaaf22704308d0a680aa156a
zz9744813-lab <zz9744813@gmail.com>
2026-05-13 03:52:33 +0800

    TASK-G: create DiffPreview.tsx

---
+++ src/components/issues/DiffPreview.tsx
@@ -0,0 +1,6 @@
+'use client'
+import React from 'react'
+
+export default function DiffPreview({ before, after }: { before: string; after: string }) {
+  return <div className="diff-preview">Diff Preview</div>
+}
