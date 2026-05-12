0131f2417fd415aadb1bd103dd431cd5a378f204
zz9744813-lab <zz9744813@gmail.com>
2026-05-13 03:52:20 +0800

    TASK-G: create CharacterSidebar.tsx

---
+++ src/components/editor/CharacterSidebar.tsx
@@ -0,0 +1,6 @@
+'use client'
+import React from 'react'
+
+export default function CharacterSidebar({ projectId }: { projectId: string }) {
+  return <aside className="character-sidebar">Character Sidebar</aside>
+}
