9550624eb702cab5fb220e28226f6c8322f64ed3
zz9744813-lab <zz9744813@gmail.com>
2026-05-13 03:52:19 +0800

    TASK-G: create BibleSidebar.tsx

---
+++ src/components/editor/BibleSidebar.tsx
@@ -0,0 +1,6 @@
+'use client'
+import React from 'react'
+
+export default function BibleSidebar({ projectId }: { projectId: string }) {
+  return <aside className="bible-sidebar">Bible Sidebar</aside>
+}
