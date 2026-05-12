106753daa93d99bcd46f3d99bcb815e84c7fe77f
zz9744813-lab <zz9744813@gmail.com>
2026-05-13 03:52:18 +0800

    TASK-G: create OutlinePanel.tsx

---
+++ src/components/editor/OutlinePanel.tsx
@@ -0,0 +1,6 @@
+'use client'
+import React from 'react'
+
+export default function OutlinePanel({ chapterId }: { chapterId: string }) {
+  return <aside className="outline-panel">Outline Panel</aside>
+}
