8e9b67d1c1f1a1e515da3c302b82baa326006c93
zz9744813-lab <zz9744813@gmail.com>
2026-05-13 03:52:32 +0800

    TASK-G: create IssueFilters.tsx

---
+++ src/components/issues/IssueFilters.tsx
@@ -0,0 +1,6 @@
+'use client'
+import React from 'react'
+
+export default function IssueFilters({ onFilter }: { onFilter: (f: any) => void }) {
+  return <div className="issue-filters">Filters</div>
+}
