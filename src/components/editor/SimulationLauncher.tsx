4736c93b0c60d880bc43ac7b3b50d411d42317a8
zz9744813-lab <zz9744813@gmail.com>
2026-05-13 03:52:24 +0800

    TASK-G: create SimulationLauncher.tsx

---
+++ src/components/editor/SimulationLauncher.tsx
@@ -0,0 +1,6 @@
+'use client'
+import React from 'react'
+
+export default function SimulationLauncher({ chapterId, projectId }: { chapterId: string; projectId: string }) {
+  return <button>Launch Simulation</button>
+}
