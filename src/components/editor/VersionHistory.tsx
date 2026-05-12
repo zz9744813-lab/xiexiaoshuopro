d8bba6df1435f433014256bdbe19afc542dd9ee2
zz9744813-lab <zz9744813@gmail.com>
2026-05-13 03:52:23 +0800

    TASK-G: create VersionHistory.tsx

---
+++ src/components/editor/VersionHistory.tsx
@@ -0,0 +1,6 @@
+'use client'
+import React from 'react'
+
+export default function VersionHistory({ chapterId }: { chapterId: string }) {
+  return <div className="version-history">Version History</div>
+}
